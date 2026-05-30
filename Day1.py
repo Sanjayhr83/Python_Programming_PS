e0ac7708000ccd6a6d6b3055f98c4ac2c545810b
Sanjay <hrsanjay@gmail.com>
2026-05-30 21:12:04 +0530

    Python Programming

--- Day1.py
+++ Day1.py
@@ -7,7 +7,7 @@
 #     return f"The largest value of the list is {l[-1]}"
 # l=eval(input("enter the number : "))
 # print(largest_val(l))
-#
+
 # # 2.remove the duplication value from the list
 # def duplication(l):
 #     out=[]
@@ -169,4 +169,26 @@
 # for i in range(1,n+1):
 #     if i not in l:
 #         print(i)
+
+# # 13.WAP to find the pair with given sum
+# """for example:
+#     l=[1000,700,100,900,300]
+#     n=1000
+#     o/p : [[1000],[700,300],[100,900]]
+# """
+# def sum(l,n):
+#     out=[]
+#     for i in range(0,len(l)):
+#         if l[i]==n:
+#             out.append([l[i]])#without square bracket it show error because we write output list inside another list
+#         else:
+#             for j in range(i+1,len(l)):
+#                 if l[i]+l[j]==n:
+#                     out.append([l[i],l[j]])
+#     return out
+# l=eval(input("enter the list : "))
+# n=int(input("enter the n value : "))
+# print(sum(l,n))
+
+
 
